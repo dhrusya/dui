5c615ca096464d18e47923ceca48feb6a4e4eadb
Dhrusya <gopal@dhrusya.com>
2018-01-18 03:41:46 +0530

    ds-cols directive update

--- dui/dui.js
+++ dui/dui.js
@@ -27,7 +27,23 @@ var DUI={};
          var sty=$(this).attr("class").split(" ");
          DUI.setGridWidth($(this),sty);
       });
-
+      $('[class*="ds-cols-"]').each( function(i,val){
+         var sty=$(this).attr("class").split(" ");
+         DUI.setColsWidth($(this),sty);
+      });
+    };
+    DUI.setColsWidth=function(ob,arr){
+      for(i=0;i<arr.length;i++){
+         var wt=DUI.getColsWidth(arr[i]);
+         var cls=arr[i].toString().trim();
+         if(wt){
+           wt=parseInt(wt);
+           wt=DUI.round(100/wt,2);
+           ob=$(".ds-row > *");
+           DUI.setElWidth(ob,wt);
+           break;
+         }
+      }
     };
     DUI.setColWidth=function(ob,arr){
       for(i=0;i<arr.length;i++){
@@ -40,7 +56,7 @@ var DUI={};
     };
     DUI.setGridWidth=function(ob,arr){
       for(i=0;i<arr.length;i++){
-        var wt=DUI.getGridWidth(arr[i]);
+        var wt=DUI.getColsWidth(arr[i], 'ds-grid-', '10', '100');
         if(wt){
           DUI.setElWidth(ob,wt);
           break;
@@ -55,6 +71,26 @@ var DUI={};
             /* stuff to do after animation is complete */
           });
     };
+    DUI.getColsWidth=function(sty, col, num, def){
+      if(!col) col="ds-cols-"; //style class name
+      if(!num) num="10"; //substr num
+      if(!def) def="1"; //default
+      var wt=false;
+      sty=sty.toString().trim();
+      if(sty.indexOf(col+"s-") > -1){
+        if(DUI.$win.width()<=480) wt= sty.toString().substr(num);
+      }else if(sty.indexOf(col+"m-") > -1){
+        if(DUI.$win.width()<=768) wt=sty.toString().substr(num);
+      }else if(sty.indexOf(col+"l-") > -1){
+        if(DUI.$win.width()<=991) wt=sty.toString().substr(num);
+      }else if(sty.indexOf(col+"xl-") > -1){
+        if(DUI.$win.width()>=1280) wt=sty.toString().substr(num+1);
+      }else if(sty.indexOf(col+"") > -1){
+        wt=sty.toString().substr(num-2);
+      }
+      return wt;
+    };
+
     DUI.getColWidth=function(sty, col){
       if(!col) col="ds-col-";
       var wt=100;
@@ -71,25 +107,16 @@ var DUI={};
       }
       return wt;
     };
-    DUI.getGridWidth=function(sty, col){
-      if(!col) col="ds-grid-";
-      var wt=false;
-      if(sty.toString().indexOf("ds-grid-s-") > -1){
-        if(DUI.$win.width()<=480) wt= sty.toString().substr(10);
-      }else if(sty.toString().indexOf("ds-grid-m-") > -1){
-        if(DUI.$win.width()<=768) wt=sty.toString().substr(10);
-      }else if(sty.toString().indexOf("ds-grid-l-") > -1){
-        if(DUI.$win.width()<=991) wt=sty.toString().substr(10);
-      }else if(sty.toString().indexOf("ds-grid-xl-") > -1){
-        if(DUI.$win.width()>=1280) wt=sty.toString().substr(11);
-      }else if(sty.toString().indexOf("ds-grid-") > -1){
-        wt=sty.toString().substr(8);
-      }
-      return wt;
+
+    DUI.round = function(number, precision) {
+        var factor = Math.pow(10, precision);
+        var tempNumber = number * factor;
+        var roundedTempNumber = Math.round(tempNumber);
+        return roundedTempNumber / factor;
     };
 
     DUI.$doc.ready(function(){
-      alert("loaded in DUI");
+      //alert("loaded in DUI");
       DUI.grid();
       DUI.$win.resize(function(event) {
         /* Act on the event */
