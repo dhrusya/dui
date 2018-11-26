15fb1920c4786be200a11a29bf6b700fbe090cf0
Gopi <gopal@dhrusya.com>
2018-11-27 01:17:18 +0530

    fixing ds-col-*

--- dui/dui.js
+++ dui/dui.js
@@ -21,7 +21,7 @@ var DUI={};
     DUI.grid=function(){
       $('.ds-row > [class*="ds-col-"]').each( function(i,val){
          var sty=$(this).attr("class").split(" ");
-
+         //console.log(sty);
          DUI.setColWidth($(this),sty);
       });
       $('.ds-row > [class*="ds-grid-"]').each( function(i,val){
@@ -37,32 +37,36 @@ var DUI={};
       for(i=0;i<arr.length;i++){
          var cls=arr[i].toString().trim();
          if(cls.length>1){
-          var wt=DUI.getColsWidth(cls);
-          if(wt){
-            wt=parseInt(wt);
-            wt=DUI.round(100/wt,2);
-            console.log(ob.attr('class'));
-            alert(ob.attr('class'));
-           var kids=ob.children();  
-            ob=$(".ds-row > *");
-            DUI.setElWidth(kids,wt);
-            break;
-          }
+           if(cls.indexOf("ds-cols-")>-1){
+             var wt=DUI.getColsWidth(cls);
+              if(wt){
+                wt=parseInt(wt);
+                wt=DUI.round(100/wt,2);
+                console.log(ob.attr('class'));
+                //alert(ob.attr('class'));
+               var kids=ob.children();
+                ob=$(".ds-row > *");
+                DUI.setElWidth(kids,wt);
+                break;
+              }
+            }
          }
-         
+
       }
     };
     DUI.setColWidth=function(ob,arr){
       for(i=0;i<arr.length;i++){
         var cls=arr[i].toString().trim();
-        if(cls.length>1){
-          var wt=DUI.getColWidth(cls);
-          if(wt){
-            DUI.setElWidth(ob,wt);
-            break;
+          if(cls.length>1){
+            if(cls.indexOf("ds-col-")>-1){
+            var wt=DUI.getColWidth(cls);
+            console.log(wt);
+            if(wt){
+              DUI.setElWidth(ob,wt);
+              break;
+            }
           }
-        }        
-        
+        }
       }
     };
     DUI.setGridWidth=function(ob,arr){
@@ -99,25 +103,26 @@ var DUI={};
         }else if(sty.indexOf(col+"") > -1){
           wt=sty.substr(num-2);
         }
-      
+
       return wt;
     };
 
     DUI.getColWidth=function(sty, col){
       if(!col) col="ds-col-";
       var wt=100;
-      sty=sty.toString().trim();      
-        if(sty.indexOf(col+"s-") > -1){
-          if(DUI.$win.width()>479) wt= sty.substr(9);
-        }else if(sty.indexOf(col+"m-") > -1){
-          if(DUI.$win.width()>767) wt=sty.substr(9);
-        }else if(sty.indexOf(col+"l-") > -1){
-          if(DUI.$win.width()>991) wt=sty.substr(9);
-        }else if(sty.indexOf(col+"xl-") > -1){
-          if(DUI.$win.width()>=1280) wt=sty.substr(10);
-        }else if(sty.indexOf(col+"") > -1){
-          wt=sty.substr(7);
-        }
+      sty=sty.toString().trim();
+      if(sty.indexOf(col+"s-") > -1){
+        if(DUI.$win.width()>479) wt= sty.substr(9);
+      }else if(sty.indexOf(col+"m-") > -1){
+        if(DUI.$win.width()>767) wt=sty.substr(9);
+      }else if(sty.indexOf(col+"l-") > -1){
+        if(DUI.$win.width()>991) wt=sty.substr(9);
+      }else if(sty.indexOf(col+"xl-") > -1){
+        if(DUI.$win.width()>=1280) wt=sty.substr(10);
+      }else if(sty.indexOf(col) > -1){
+        wt=sty.substr(7);
+      }
+
       return wt;
     };
 
